36c3c0cfd77fc6b6fc03057c8b3091cb879ec8c4
Abhay-87 <49601394+Abhay-87@users.noreply.github.com>
2023-06-30 15:26:32 +0530

    Uploading a dead token
    Uploading a dead token to check Github Secret Scanning integration

---
+++ GithubScanTestV2.java
@@ -0,0 +1,12 @@
+package string;
+
+public class GithubScanTestV2 {
+    private static String LM_TOKEN = "lmb_VmhHeTVnbUpJWW40ODUzbXVxZEo6eHBBb3BoV01ybFdCY2tUZXhXdWxFQT09LNjNkNTBmOGUtZmQwNC00OGRhLTg5MTUtNTg5OWUzMWU3YWIwL0wZ448";
+    public static void main(String[] args) {
+        useToken();
+    }
+    private static void useToken() {
+        System.out.println("Using token");
+        System.out.println(LM_TOKEN);
+    }
+}
